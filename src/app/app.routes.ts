90ce4388687ac6d02cb4f2acd643df11fa47cf94
Erick Bueno <erickjb93@gmail.com>
2025-01-06 09:57:46 -0300

    feat: add login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"/login",
+        component: LoginScreenComponent,
+        loadChildren: () => import("./components/login-screen/login-screen.component").then(l => l.LoginScreenComponent)
+    }
+];
