f0481d501fb91bdd8eedbb687ae5683de5226244
Erick Bueno <erickjb93@gmail.com>
2025-01-06 10:42:44 -0300

    feat: add lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
 export const routes: Routes = [
     {
-        path:"/login",
-        component: LoginScreenComponent,
-        loadChildren: () => import("./components/login-screen/login-screen.component").then(l => l.LoginScreenComponent)
+       path: "",
+       loadChildren: () => import("./components/auth/auth.routes").then(l => l.routes)
     }
 ];
