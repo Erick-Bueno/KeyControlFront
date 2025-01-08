1cf80475b91cfda1a2f8f27dbf6dec765997fc84
Erick bueno <erickjb93@gmail.com>
2025-01-08 09:39:06 -0300

    ✨ feat: add input password component

--- src/app/components/auth/login-screen/login-screen.component.ts
+++ src/app/components/auth/login-screen/login-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputEmailComponent } from '@components/common/input-email/input-email.component';
+import { InputPasswordComponent } from "../../common/input-password/input-password.component";
 
 
 @Component({
   selector: 'app-login-screen',
-  imports: [InputEmailComponent],
+  imports: [InputEmailComponent, InputPasswordComponent],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.css'
 })
