67c1853cd6e3d49fe49061e2080cb0f43c48baa9
Erick bueno <erickjb93@gmail.com>
2025-01-09 11:42:41 -0300

    feat: add control value acessor for input email

--- src/app/components/common/input-email/input-email.component.ts
+++ src/app/components/common/input-email/input-email.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input-email',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-email.component.html',
-  styleUrl: './input-email.component.css'
+  styleUrl: './input-email.component.css',
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputEmailComponent),
+      multi: true
+    }
+  ]
 })
-export class InputEmailComponent {
+export class InputEmailComponent implements  ControlValueAccessor {
+  value = '';
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onChange: (value: string) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onTouched: () => void = () => {};
+
+  writeValue(obj: string | undefined): void {
+    if (obj !== undefined) {
+      this.value = obj;
+    }
+  }
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  updateValue(): void {
+    this.onChange(this.value);
+  }
 
 }
