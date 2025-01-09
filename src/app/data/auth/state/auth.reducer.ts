22f0e8e4ff287c9a7afe97c327a6970ca68f0477
Erick Bueno <erickjb93@gmail.com>
2025-01-09 07:19:35 -0300

    feat: add failure and success reducer for auth

--- src/app/data/auth/state/auth.reducer.ts
+++ src/app/data/auth/state/auth.reducer.ts
@@ -1,8 +1,41 @@
 import { LoginStats } from "@enums/login-stats";
 import { LoginResponse } from "../responses/login-response";
+import { createReducer, on } from "@ngrx/store";
+import { authActions } from "./auth.action";
 
 export interface LoginState {
-    user: LoginResponse,
-    errors: '' | null
-    status: LoginStats
+    user: LoginResponse | null,
+    error: string | null
+    status: LoginStats 
 }
+
+const initialState:LoginState = {
+    error: null,
+    status: LoginStats.pending,
+    user: null
+}
+
+export const AuthReducer = createReducer(
+    initialState,
+
+    on(authActions.login, (state) => ({
+        ...state,
+        status: LoginStats.loading,
+        error: null,
+        user: null
+    })),
+
+    on(authActions.loginSuccess, (state, {user}) => ({
+        ...state,
+        status: LoginStats.success,
+        error: null,
+        user: user
+    })),
+
+    on(authActions.loginFailure, (state, {error}) => ({
+        ...state,
+        status: LoginStats.success,
+        error: error,
+        user: null
+    }))
+)
