dd7512e3ebbb43e1d29024983fd60e2205994939
Erick Bueno <erickjb93@gmail.com>
2025-01-09 07:19:11 -0300

    feat: add failure and success action for auth

--- src/app/data/auth/state/auth.action.ts
+++ src/app/data/auth/state/auth.action.ts
@@ -1,11 +1,24 @@
 import { createAction, props } from "@ngrx/store";
+import { LoginResponse } from "../responses/login-response";
 
 
 const login = createAction(
-    '[Auth] Login', 
+    '[Auth] Login',
     props<{ email: string, password: string }>()
 )
 
+const loginSuccess = createAction(
+    '[Auth] Login Success',
+    props<{ user: LoginResponse }>()
+);
+
+const loginFailure = createAction(
+    '[Auth] Login Failure',
+    props<{ error: string }>()
+)
+
 export const authActions = {
-    login
+    login,
+    loginSuccess,
+    loginFailure
 }
