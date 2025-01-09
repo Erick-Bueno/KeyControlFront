a9d62303dc16f03f1a1eaedc1c91183e8b9cc4e5
Erick Bueno <erickjb93@gmail.com>
2025-01-09 05:44:14 -0300

    feat: add authentication method

--- src/app/components/auth/login-screen/login-screen.component.ts
+++ src/app/components/auth/login-screen/login-screen.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputEmailComponent } from '@components/common/input-email/input-email.component';
 import { InputPasswordComponent } from "../../common/input-password/input-password.component";
+import { ButtonAuthComponent } from "../../common/button-auth/button-auth.component";
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
 
 
 @Component({
   selector: 'app-login-screen',
-  imports: [InputEmailComponent, InputPasswordComponent],
+  imports: [InputEmailComponent, InputPasswordComponent, ButtonAuthComponent],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.css'
 })
 export class LoginScreenComponent {
-
+  buttonText = "Login"
+  store = inject(Store)
+  router = inject(Router)
+  authentication(){
+    const currentUrl = this.router.url;
+    if(currentUrl.includes('login')){
+      console.log("login")
+    }
+  }
 }
