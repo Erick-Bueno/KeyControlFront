69c00e770ae3fb3fe5a186fe8041ed1966f12282
Erick bueno <erickjb93@gmail.com>
2025-01-09 11:31:29 -0300

    feat: register effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideHttpClient } from '@angular/common/http';
+import { appReducers } from './state/app.reducers';
+import { provideEffects } from '@ngrx/effects';
+import { loginEffect, saveUserInCookiesEffect } from '@states/auth/state/auth.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(appReducers), provideHttpClient(),
+  provideEffects([
+    { loginEffect },
+    { saveUserInCookiesEffect }
+  ])
+  ]
 };
