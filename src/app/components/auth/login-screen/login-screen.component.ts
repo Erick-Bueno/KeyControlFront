d110248d9d76b3ded6cf5c0610b5004d6d05b33c
Erick bueno <erickjb93@gmail.com>
2025-01-09 11:34:03 -0300

    refact: alter method submit form name

--- src/app/components/auth/login-screen/login-screen.component.ts
+++ src/app/components/auth/login-screen/login-screen.component.ts
@@ -6,12 +6,14 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { authActions } from '@states/auth/state/auth.action';
+import { loginErrorSelector } from '@states/auth/state/auth.selectors';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-login-screen',
-  imports: [InputEmailComponent, InputPasswordComponent, ButtonAuthComponent, ReactiveFormsModule],
+  imports: [InputEmailComponent, InputPasswordComponent, ButtonAuthComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.css'
 })
@@ -20,11 +22,12 @@ export class LoginScreenComponent {
   private store = inject(Store)
   private router = inject(Router)
   private formBuilder = inject(NonNullableFormBuilder)
+  errorMessage$ = this.store.select(loginErrorSelector);
   protected form = this.formBuilder.group({
     email: [null, [Validators.required, Validators.email]],
     password: [null, [Validators.required, Validators.pattern("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-_@#$%]).{8,}")]]
   })
-  authentication() {
+  submit() {
     const currentUrl = this.router.url;
     if (currentUrl.includes('login')) {
       this.store.dispatch(authActions.login({email: this.form.get("email")?.value ?? "", password: this.form.get("password")?.value ?? ""}))
