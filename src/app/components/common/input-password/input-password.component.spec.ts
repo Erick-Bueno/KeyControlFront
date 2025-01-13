e69e85991debe0739e8ad617d2f8d18ee3968e81
Erick bueno <erickjb93@gmail.com>
2025-01-13 15:58:22 -0300

    test: add when click in eye should alter visibility in input test

--- src/app/components/common/input-password/input-password.component.spec.ts
+++ src/app/components/common/input-password/input-password.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputPasswordComponent } from './input-password.component';
+import { By } from '@angular/platform-browser';
+import { NgControl } from '@angular/forms';
+
+
 
 describe('InputPasswordComponent', () => {
   let component: InputPasswordComponent;
   let fixture: ComponentFixture<InputPasswordComponent>;
+  let ngControlMock: jasmine.SpyObj<NgControl>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InputPasswordComponent]
+      imports: [InputPasswordComponent],
+      providers: [{ provide: NgControl, useValue: ngControlMock }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(InputPasswordComponent);
     component = fixture.componentInstance;
@@ -20,4 +26,20 @@ describe('InputPasswordComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it("when click in eye should alter visibility in input password", () => {
+    const eyeIcon = fixture.debugElement.query(By.css('.olho'));
+    const inputPassword = fixture.debugElement.query(By.css('input.input-password'));
+
+    expect(inputPassword.nativeElement.type).toBe('password');
+
+    eyeIcon.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(inputPassword.nativeElement.type).toBe('text');
+
+    eyeIcon.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(inputPassword.nativeElement.type).toBe('password');
+  })
 });
